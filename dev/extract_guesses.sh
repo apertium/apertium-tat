cb8e555bb52227492c650bd8d2b7b640d6a8ddef
Jonathan North Washington <jonathan.n.washington@gmail.com>
2013-10-28 18:43:17 +0000

    better extract script

--- dev/extract_guesses.sh
+++ dev/extract_guesses.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-IFS= read -r var
-
-string=`echo $var | hfst-proc ../tat.automorf.hfst | cg-conv -a | vislcg3 --grammar metrics.rlx`;
-IFS=$'\n';
-for line in $string; do
-	guess=`echo $line | grep "guess_"`;
-	word=`echo $guess | sed -E 's/.*\"\*(.*)\".*_([A-Z\-]*)/\1:\1 NP-\2 ; ! ""/'`;
-	if [ ! "$word" == "" ]; then
-		echo $word;
-	fi;
+while IFS= read -r line; do
+	string=`echo $line | hfst-proc ../tat.automorf.hfst | cg-conv -a 2>/dev/null | vislcg3 --grammar metrics.rlx`;
+	IFS=$'\n';
+	for fooline in $string; do
+		guess=`echo $fooline | grep "guess_"`;
+		word=`echo $guess | sed -E 's/.*\"\*(.*)\".*_([A-Z\-]*)/\1:\1 NP-\2 ; ! ""/'`;
+		if [ ! "$word" == "" ]; then
+			echo $word;
+		fi;
+	done;
 done;

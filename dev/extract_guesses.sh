193c5d4cab67b43bb293b58168b0a7c9270f6b34
Jonathan North Washington <jonathan.n.washington@gmail.com>
2013-10-28 19:58:05 +0000

    other updates

--- dev/extract_guesses.sh
+++ dev/extract_guesses.sh
@@ -8,7 +8,7 @@ while IFS= read -r line; do
 		guess=`echo $fooline | grep "guess_"`;
 		word=`echo $guess | sed -E 's/.*\"\*(.*)\".*_([A-Z\-]*)/\1:\1 NP-\2 ; ! ""/'`;
 		if [ ! "$word" == "" ]; then
-			echo $word;
+			echo $word $line;
 		fi;
 	done;
 done;

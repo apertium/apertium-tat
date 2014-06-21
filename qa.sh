bfb9288a2e2d465f92ad76b2f0cce9ed042c646f
Ilnar Salimzyan <ilnar@selimcan.org>
2014-06-21 19:11:55 +0000

    separate tat.test test script from actual test cases

--- qa.sh
+++ qa.sh
@@ -1,6 +1,15 @@
-# Takes the basename of the test scrpt in /tests as an argument
-# and runs the test.
-
 #!/bin/bash
 
-bash "tests/$1.test"
+# Takes the basename of the test scrpt in /test-scripts as an argument,
+# an additional argument if the test script requires it, and runs the test.
+#
+# Usage: ./qa.sh (will default to './qa.sh tat')
+
+if [ $# -eq 0 ]
+then
+    testToRun=tat.test
+else
+    testToRun=$1.test
+fi
+
+bash "test-scripts/$testToRun" "$2"

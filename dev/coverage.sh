bab235117359b5a8fd784e790fdf89b42621a4c0
Ilnar Salimzyan <ilnar@selimcan.org>
2017-04-05 21:39:07 +0000

    minor

--- dev/coverage.sh
+++ dev/coverage.sh
@@ -1,1 +1,7 @@
-for f in `find ~/src/turkiccorpora/ -type f -name "tat.*.txt"`; do printf "Corpus: $f\n"; aq-covtest $f tat.automorf.bin; printf "\n"; done > dev/coverage.txt
+export PYTHONPATH=/usr/local/lib/python3.5/site-packages/:$PYTHONPATH
+
+for f in `find ~/src/turkiccorpora/ -type f -name "tat.*.txt"`; do
+    printf "Corpus: $f\n"
+    aq-covtest $f tat.automorf.bin
+    printf "\n";
+done > dev/coverage.txt

cded8386774d825655b43ee5766f6b9a4c899ffb
Ilnar Salimzianov <ilnar@selimcan.org>
2019-03-13 19:03:07 +0300

    add pkg info

---
+++ info.rkt
@@ -0,0 +1,7 @@
+#lang info
+(define collection "apertium-tat")
+(define deps '("base" "rash"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define pkg-desc "Morphological transducer and disambiguator for Tatar")
+(define version "0.0")
+(define pkg-authors '())
---
+++ main.rkt
@@ -0,0 +1,29 @@
+#lang racket
+
+; Racket interface for apertium-tat
+;
+; REQUIRES: apertiumpp package.
+; https://taruen.github.io/apertiumpp/apertiumpp/ gives info on how to install
+; it.
+
+(provide tat-morph)
+
+(require pkg/lib
+         rackunit
+         rash
+         apertiumpp/streamparser)
+
+(define (symbol-append s1 s2)
+  (string->symbol (string-append (symbol->string s1) (symbol->string s2))))
+
+(define A-TAT './)
+
+(define (tat-morph s)
+  (parameterize ([current-directory (pkg-directory "apertium-tat")])
+    (rash
+     "echo (values s) | apertium -n -d . tat-morph")))
+
+(define (tat-disam s)
+  (parameterize ([current-directory (pkg-directory "apertium-tat")])
+    (rash
+     "echo (values s) | apertium -n -d . tat-disam | cg-conv -A -l")))
